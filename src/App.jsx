3324a58575f0f7eaafcd8cebc65cb01b0624d989
gihantha <gihantha01@gmail.com>
2025-03-31 15:53:31 +0530

    create a simple form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React, {useState} from 'react';
 import './App.css';
-import ChildComponent1 from "./components/ChildComponent1.jsx";
 const App = () => {
-    const [myName, setMyName] = useState('kaveen');
-    console.log(myName);
   return (
       <div>
         <h1>React Component</h1>
-        <ChildComponent1
-            name={myName}
-            setMyName={setMyName}
-        />
+          <form action="">
+              <label htmlFor="">First Name :</label>
+              <input type="text" placeholder="Enter Your First Name"/>
+              <br/>
+              <label htmlFor="">Last Name :</label>
+              <input type="text" placeholder="Enter Your Last Name"/>
+              <br/>
+              <label htmlFor="">Email :</label>
+              <input type="email" placeholder="Enter Your Email"/>
+              <br/>
+              <label htmlFor="">Password :</label>
+              <input type="password" placeholder="Enter Your Password"/>
+          </form>
       </div>
   );
 };
