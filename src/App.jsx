231f7f39db42ff854369aa05c96017b3cde81bdb
gihantha <gihantha01@gmail.com>
2025-03-31 11:29:29 +0530

    project initializing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ChildComponent1 from "./components/ChildComponent1.jsx";
+const App = () => {
+  return (
+      <div>
+        <h1>React Component</h1>
+        <ChildComponent1/>
+      </div>
+  );
+};
+
+export default App;
---
+++ src/components/ChildComponent1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChildComponent1 = () => {
+    return (
+        <div>
+            Child Com 1
+        </div>
+    );
+};
+
+export default ChildComponent1;
